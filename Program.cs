0fc1af1f42e0b2527fdf5460ec4f47cd98c0f65c
mrhl082024 <mrhl082024@gmail.com>
2024-10-29 10:03:58 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_29._10._24;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ model/model.cs
@@ -0,0 +1,10 @@
+public class Model
+{
+    public string? Name { get; set; }
+    public int? Age { get; set; }
+
+    public Model()
+    {
+
+    }
+}
